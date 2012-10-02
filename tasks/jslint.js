b0e15eaae199582a4b978d74756406c5e76a7ce4
Stephen Mathieson <smath23@gmail.com>
2012-10-02 14:31:09 -0400

    Minor cleanup, adding `errorsOnly` support

--- tasks/jslint.js
+++ tasks/jslint.js
@@ -7,19 +7,20 @@
  */
 
 /*jslint node:true*/
-/*jslint stupid:true*/
 /*jslint nomen:true*/
 
 module.exports = function (grunt) {
+
 	'use strict';
 
-	var jslint, template, templateString, jUnitTemplate,
+	var jslint, template, templateStandard, jUnitTemplate, templateOnlyErrors,
 		vm = require('vm'),
 		fs = require('fs'),
 		ctx = vm.createContext();
 
 	/**
 	 * Checks to see if a thing is neither undefired nor null
+	 *
 	 * @param  {Mixed|Any}  thing The thing to check
 	 * @return {Boolean}          True if the thing is either null or undefined
 	 */
@@ -27,10 +28,13 @@ module.exports = function (grunt) {
 		return thing !== undefined && thing !== null;
 	}
 
-	templateString = grunt.file.read(__dirname + '/templates/standard.tmpl');
-
+	/*jslint nomen:true, stupid:true*/
+	templateStandard = grunt.file.read(__dirname + '/templates/standard.tmpl');
+	templateOnlyErrors = grunt.file.read(__dirname + '/templates/errors-only.tmpl');
+	jUnitTemplate = grunt.file.read(__dirname + '/templates/junit.tmpl');
 
 	vm.runInContext(fs.readFileSync(__dirname + '/JSLint/jslint.js'), ctx);
+	/*jslint nomen:false, stupid:false*/
 
 	jslint = ctx.JSLINT;
 
@@ -40,7 +44,6 @@ module.exports = function (grunt) {
 			directives = grunt.config('jslint_directives') || {},
 			options = grunt.config('jslint_options') || {},
 			files = grunt.file.expandFiles(this.file.src),
-			fileCount = files.length,
 			filesInViolation = 0,
 			errorCount = 0,
 			report = {
@@ -71,6 +74,7 @@ module.exports = function (grunt) {
 				errorCount += errors.length;
 				filesInViolation += 1;
 			}
+
 			report.files[index] = {
 				filepath: filepath,
 				passed: passed,
@@ -82,7 +86,15 @@ module.exports = function (grunt) {
 		report.failures = errorCount;
 		report.filesInViolation = filesInViolation;
 
-		template = grunt.template.process(templateString, report);
+		if (options.errorsOnly) {
+
+			template = grunt.template.process(templateOnlyErrors, report);
+
+		} else {
+
+			template = grunt.template.process(templateStandard, report);
+
+		}
 
 		grunt.log.write(template);
 
@@ -91,8 +103,6 @@ module.exports = function (grunt) {
 		}
 
 		if (options.junit) {
-
-			jUnitTemplate = grunt.file.read(__dirname + '/templates/junit.tmpl');
 			template = grunt.template.process(jUnitTemplate, report);
 
 			grunt.file.write(options.junit, template);
