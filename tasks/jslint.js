1f4b165b244c2d0c4fe92e3fd3f8a30e20f57f1c
Stephen Mathieson <smath23@gmail.com>
2012-12-21 08:54:12 -0500

    0.2.4: refactor to add test suite

--- tasks/jslint.js
+++ tasks/jslint.js
@@ -7,30 +7,13 @@
  */
 
 /*jslint node:true*/
-/*jslint nomen:true*/
 
-var vm = require('vm');
-var fs = require('fs');
-var entities = require('entities');
-var clone = require('clone');
-var underscore = require('underscore');
+var reports = require('../lib/reports.js');
 
-var ctx = vm.createContext();
+var runner = require('../lib/runner.js');
 
 module.exports = function (grunt) {
 	'use strict';
-
-	var jslint,
-		templates = {};
-
-	vm.runInContext(grunt.file.read(__dirname + '/JSLint/jslint.js'), ctx);
-
-	jslint = ctx.JSLINT;
-
-	templates.standard = grunt.file.read(__dirname + '/templates/standard.tmpl');
-	templates.errors_only = grunt.file.read(__dirname + '/templates/errors-only.tmpl');
-	templates.junit = grunt.file.read(__dirname + '/templates/junit-xml.tmpl');
-	templates.jslint_xml = grunt.file.read(__dirname + '/templates/jslint-xml.tmpl');
 
 	/**
 	 * Grabs a config option from the jslint namespace
@@ -43,30 +26,15 @@ module.exports = function (grunt) {
 	}
 
 	/**
-	 * Checks to see if a thing is neither undefired nor null
-	 *
-	 * @param  {Mixed|Any}  thing The thing to check
-	 * @return {Boolean}          True if the thing is either null or undefined
-	 */
-	function isDefined(thing) {
-		return thing !== undefined && thing !== null;
-	}
-
-	/**
 	 * The task
 	 */
 	grunt.registerTask('jslint', 'Validate JavaScript files with JSLint', function () {
 
-		var template,
+		var next = this.async(),
 			files = conf('files'),
 			excludedFiles = conf('exclude') || [],
 			options = conf('options') || {},
-			directives = conf('directives') || {},
-			filesInViolation = 0,
-			errorCount = 0,
-			report = {
-				files: []
-			};
+			directives = conf('directives') || {};
 
 		if (!files) {
 			grunt.log.error('NO FILES?!?');
@@ -79,87 +47,47 @@ module.exports = function (grunt) {
 
 		excludedFiles = grunt.file.expandFiles(excludedFiles);
 
-		files = grunt.file.expandFiles(files).filter(function (file) {
-			return excludedFiles.indexOf(file) === -1;
-		});
+		files = grunt.file
+			.expandFiles(files)
+			.filter(function (file) {
+				return excludedFiles.indexOf(file) === -1;
+			});
 
-		files.forEach(function (filepath, index) {
+		runner(files, directives, function (err, report) {
 
-			var source = grunt.file.read(filepath),
-				passed = jslint(source, directives),
-				data = jslint.data(),
-				errors = [],
-				encodedErrors = [];
+			var template;
 
-
-			errors = errors.concat(jslint.errors);
-			if (!directives.unused) {
-				errors = errors.concat(data.unused);
-			}
-			errors = errors.filter(isDefined);
-
-			if (errors.length) {
-				errorCount += errors.length;
-				filesInViolation += 1;
-
-				errors.forEach(function (error) {
-					var encodedError = clone(error),
-						evidence = error.evidence,
-						reason = error.reason;
-
-					encodedError.evidence = evidence ? entities.encode(evidence, 0) : evidence;
-					encodedError.reason = reason ? entities.encode(reason, 0) : reason;
-					encodedErrors.push(encodedError);
-				});
+			if (err) {
+				grunt.log.error(err);
+				return next(false);
 			}
 
-			report.files[index] = {
-				filepath: filepath,
-				passed: passed,
-				errors: errors,
-				encodedErrors: encodedErrors
-			};
+			if (options.errorsOnly) {
+				template = reports.errorsOnly(report);
+			} else {
+				template = reports.standard(report);
+			}
 
+			if (options.log) {
+				grunt.file.write(options.log, grunt.log.uncolor(template));
+			}
+
+			if (options.junit) {
+				grunt.file.write(options.junit, reports.junitXml(report));
+			}
+
+			if (options.jslintXml) {
+				grunt.file.write(options.jslintXml, reports.jslintXml(report));
+			}
+
+			grunt.log.write(template);
+
+			if (report.failures) {
+				next(false);
+			} else {
+				next(true);
+			}
 		});
-
-		report.failures = errorCount;
-		report.filesInViolation = filesInViolation;
-
-		if (options.errorsOnly) {
-			template = underscore.template(templates.errors_only, {
-				'report': report
-			});
-		} else {
-			template = underscore.template(templates.standard, {
-				'report': report
-			});
-		}
-
-		grunt.log.write(template);
-
-		if (options.log) {
-			grunt.file.write(options.log, grunt.log.uncolor(template));
-		}
-
-		if (options.junit) {
-			template = underscore.template(templates.junit, {
-				'report': report
-			});
-
-			grunt.file.write(options.junit, template);
-		}
-
-		if (options.jslintXml) {
-			template = underscore.template(templates.jslint_xml, {
-				'report': report
-			});
-
-			grunt.file.write(options.jslintXml, template);
-		}
-
-		if (errorCount && options.failOnError) {
-			return false;
-		}
 
 	});
 
