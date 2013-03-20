50c0ab12c40f37c682eaebaf5439d3d07ea9318d
Stephen Mathieson <smath23@gmail.com>
2013-03-20 10:03:06 -0400

    convert to spaces; keep crockford happy

--- tasks/jslint.js
+++ tasks/jslint.js
@@ -8,97 +8,96 @@
 
 /*jslint node:true*/
 
-var reports = require('../lib/reports.js');
+'use strict';
 
-var runner = require('../lib/runner.js');
+var reports = require('../lib/reports'),
+  runner = require('../lib/runner');
 
 module.exports = function (grunt) {
-	'use strict';
+  /**
+   * Grabs a config option from the jslint namespace
+   *
+   * @param  {String} option The option/configuration key
+   * @return {Mixed|Any}     The key's value
+   */
+  function conf(option) {
+    return grunt.config('jslint.' + option);
+  }
 
-	/**
-	 * Grabs a config option from the jslint namespace
-	 *
-	 * @param  {String} option The option/configuration key
-	 * @return {Mixed|Any}     The key's value
-	 */
-	function conf(option) {
-		return grunt.config('jslint.' + option);
-	}
+  /**
+   * The task
+   */
+  grunt.registerTask('jslint', 'Validate JavaScript files with JSLint', function () {
 
-	/**
-	 * The task
-	 */
-	grunt.registerTask('jslint', 'Validate JavaScript files with JSLint', function () {
+    var next = this.async(),
+      files = conf('files'),
+      excludedFiles = conf('exclude') || [],
+      options = conf('options') || {},
+      directives = conf('directives') || {};
 
-		var next = this.async(),
-			files = conf('files'),
-			excludedFiles = conf('exclude') || [],
-			options = conf('options') || {},
-			directives = conf('directives') || {};
+    if (!files) {
+      grunt.log.error('NO FILES?!?');
+      return false;
+    }
 
-		if (!files) {
-			grunt.log.error('NO FILES?!?');
-			return false;
-		}
+    if (options.failOnError === undefined) {
+      options.failOnError = true;
+    }
 
-		if (options.failOnError === undefined) {
-			options.failOnError = true;
-		}
+    excludedFiles = grunt.file.expand(excludedFiles);
 
-		excludedFiles = grunt.file.expand(excludedFiles);
+    files = grunt.file
+      .expand(files)
+      .filter(function (file) {
+        return excludedFiles.indexOf(file) === -1;
+      });
 
-		files = grunt.file
-			.expand(files)
-			.filter(function (file) {
-				return excludedFiles.indexOf(file) === -1;
-			});
+    options.directives = directives;
 
-		options.directives = directives;
+    runner(files, options, function (err, report) {
 
-		runner(files, options, function (err, report) {
+      var template;
 
-			var template;
+      if (err) {
+        grunt.log.error(err);
+        return next(false);
+      }
 
-			if (err) {
-				grunt.log.error(err);
-				return next(false);
-			}
+      if (options.errorsOnly) {
+        template = reports.errorsOnly(report);
+      } else {
+        template = reports.standard(report);
+      }
 
-			if (options.errorsOnly) {
-				template = reports.errorsOnly(report);
-			} else {
-				template = reports.standard(report);
-			}
+      if (options.log) {
+        grunt.file.write(options.log, grunt.log.uncolor(template));
+      }
 
-			if (options.log) {
-				grunt.file.write(options.log, grunt.log.uncolor(template));
-			}
+      if (options.junit) {
+        grunt.file.write(options.junit, reports.junitXml(report));
+      }
 
-			if (options.junit) {
-				grunt.file.write(options.junit, reports.junitXml(report));
-			}
+      if (options.jslintXml) {
+        grunt.file.write(options.jslintXml, reports.jslintXml(report));
+      }
 
-			if (options.jslintXml) {
-				grunt.file.write(options.jslintXml, reports.jslintXml(report));
-			}
+      if (options.checkstyle) {
+        grunt.file.write(options.checkstyle, reports.checkstyle(report));
+      }
 
-			if (options.checkstyle) {
-				grunt.file.write(options.checkstyle, reports.checkstyle(report));
-			}
+      if (grunt.option('no-color')) {
+        template = grunt.log.uncolor(template);
+      }
 
-			if (grunt.option('no-color')) {
-				template = grunt.log.uncolor(template);
-			}
+      grunt.log.write(template);
 
-			grunt.log.write(template);
+      if (report.failures && options.failOnError) {
+        next(false);
+      } else {
+        next(true);
+      }
+    });
 
-			if (report.failures && options.failOnError) {
-				next(false);
-			} else {
-				next(true);
-			}
-		});
-
-	});
+  });
 
 };
