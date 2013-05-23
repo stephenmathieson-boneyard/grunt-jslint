b419b5c4db381365341b04f01151f9d9a8d98c37
Stephen Mathieson <smath23@gmail.com>
2013-05-23 11:35:03 -0400

    lint

--- test/colors.js
+++ test/colors.js
@@ -15,52 +15,52 @@ var suite = vows.describe('colors');
  */
 function test(color) {
 
-	var string = 'hi';
+  var string = 'hi';
 
-	// not enumerable
-	assert.isFalse(string.hasOwnProperty(color));
+  // not enumerable
+  assert.isFalse(string.hasOwnProperty(color));
 
-	// existance
-	assert.ok(string[color]);
+  // existance
+  assert.ok(string[color]);
 
-	// keeps the string
-	assert.include(string[color], string);
+  // keeps the string
+  assert.include(string[color], string);
 
 }
 
 suite.addBatch({
-	'additions': {
-		topic: function () {
-			return require('../lib/colors.js');
-		},
-		'should add "black"': function () {
-			test('black');
-		},
-		'should add "blue"': function () {
-			test('blue');
-		},
-		'should add "cyan"': function () {
-			test('cyan');
-		},
-		'should add "green"': function () {
-			test('green');
-		},
-		'should add "grey"': function () {
-			test('grey');
-		},
-		'should add "magenta"': function () {
-			test('magenta');
-		},
-		'should add "red"': function () {
-			test('red');
-		},
-		'should add "white"': function () {
-			test('white');
-		},
-		'should add "yellow"': function () {
-			test('yellow');
-		}
-	}
+  'additions': {
+    topic: function () {
+      return require('../lib/colors.js');
+    },
+    'should add "black"': function () {
+      test('black');
+    },
+    'should add "blue"': function () {
+      test('blue');
+    },
+    'should add "cyan"': function () {
+      test('cyan');
+    },
+    'should add "green"': function () {
+      test('green');
+    },
+    'should add "grey"': function () {
+      test('grey');
+    },
+    'should add "magenta"': function () {
+      test('magenta');
+    },
+    'should add "red"': function () {
+      test('red');
+    },
+    'should add "white"': function () {
+      test('white');
+    },
+    'should add "yellow"': function () {
+      test('yellow');
+    }
+  }
 });
 
 suite.export(module);
