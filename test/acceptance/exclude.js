dbe8ac2cba6c94905f091c28603f8c5f04e553b8
Stephen Mathieson <smath23@gmail.com>
2014-03-19 13:21:14 -0400

    lint

--- test/acceptance/exclude.js
+++ test/acceptance/exclude.js
@@ -7,19 +7,21 @@ var assert = require('better-assert');
 
 var out = '';
 
-spawn('grunt', [ 'jslint' ], {
+var opts = {
   cwd: path.join(__dirname, '..', '..', 'examples', 'exclude')
-})
-.on('close', function (code) {
-  // no errors
-  assert(code === 0);
-  // contains
-  assert(out.indexOf('Done, without errors.') !== -1);
-})
-.stdout.on('data', function (data) {
-  data = data.toString();
-  out += data;
-  if (process.argv.indexOf('--verbose') !== -1) {
-    process.stdout.write(data);
-  }
-});
+};
+
+spawn('grunt', [ 'jslint' ], opts)
+  .on('close', function (code) {
+    // no errors
+    assert(code === 0);
+    // contains
+    assert(out.indexOf('Done, without errors.') !== -1);
+  })
+  .stdout.on('data', function (data) {
+    data = data.toString();
+    out += data;
+    if (process.argv.indexOf('--verbose') !== -1) {
+      process.stdout.write(data);
+    }
+  });
